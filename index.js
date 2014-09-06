a264cbe95b8337dc2e9810dc15c64d87b6556ac7
Jason Frame <jason@onehackoranother.com>
2014-09-06 11:07:10 +0100

    massive reorganisation

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-x(require('./lib/defaults'));
-x(require('./lib/functional'));
-x(require('./lib/object'));
-x(require('./lib/string'));
-
-function x(mod) {
-    for (var k in mod) {
-        exports[k] = mod[k];
-    }
-}
-
-[   'all',
-	'any',
-	'mapValues',
-    'append',
-    'prepend',
-    'pluck'
-].forEach(function(fn) {
-    exports[fn].C = exports.autoCurry(exports[fn]);
+[	require('./lib/curry'),
+	require('./lib/defaults'),
+	require('./lib/dom'),
+	require('./lib/functional'),
+	require('./lib/indexing'),
+	require('./lib/string')
+].forEach(function(lib) {
+	for (var k in lib) {
+		exports[k] = lib[k];
+	}
 });
