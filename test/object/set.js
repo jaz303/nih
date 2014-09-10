ec753aff249a2311ec9c9869c7bdc0e8ff1b02ea
Jason Frame <jason@onehackoranother.com>
2014-09-10 14:55:36 +0100

    test set with objects too

--- test/object/set.js
+++ test/object/set.js
@@ -1,9 +1,18 @@
 var test = require('tape');
 var set = require('../..').set;
 
-test("set makes set", function(assert) {
+test("set - array", function(assert) {
 
     var res = set(['a', 'b', 'c']);
+
+    assert.deepEqual(res, {a: true, b: true, c: true});
+    assert.end();
+
+});
+
+test("set - object", function(assert) {
+
+    var res = set({ x: 'a', y: 'b', z: 'c' });
 
     assert.deepEqual(res, {a: true, b: true, c: true});
     assert.end();
